1e115f937ae938b11374aeab600219195f0d9f35
Nate Smith <nwjsmith@gmail.com>
2019-04-10 09:42:24 -0400

    Use character literals for better readability

--- test/lambdaisland/deep_diff/printer_test.clj
+++ test/lambdaisland/deep_diff/printer_test.clj
@@ -12,10 +12,10 @@
 
 (deftest print-doc-test
   (testing "date"
-    (is (= "[31m-#inst \"2019-04-09T14:57:46.128-00:00\"[0m [32m+#inst \"2019-04-10T14:57:46.128-00:00\"[0m\n"
+    (is (= "\u001B[31m-#inst \"2019-04-09T14:57:46.128-00:00\"\u001B[0m \u001B[32m+#inst \"2019-04-10T14:57:46.128-00:00\"\u001B[0m\n"
            (printed (diff/diff #inst "2019-04-09T14:57:46.128-00:00"
                                #inst "2019-04-10T14:57:46.128-00:00")))))
   (testing "uuid"
-    (is (= "[31m-#uuid \"e41b325a-ce9d-4fdd-b51d-280d9c91314d\"[0m [32m+#uuid \"0400be9a-619f-4c6a-a735-6245e4955995\"[0m\n"
+    (is (= "\u001B[31m-#uuid \"e41b325a-ce9d-4fdd-b51d-280d9c91314d\"\u001B[0m \u001B[32m+#uuid \"0400be9a-619f-4c6a-a735-6245e4955995\"\u001B[0m\n"
            (printed (diff/diff #uuid "e41b325a-ce9d-4fdd-b51d-280d9c91314d"
                                #uuid "0400be9a-619f-4c6a-a735-6245e4955995"))))))
