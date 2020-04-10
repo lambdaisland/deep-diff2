839ffa9919dcb646dae84764e2e50b8b6feec47c
Arne Brasseur <arne@arnebrasseur.net>
2020-04-10 14:40:33 +0200

    Setup shadow+chui to demo chui

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,12 @@
-((nil . ((cider-clojure-cli-global-options     . "-A:dev:test")
-         (cider-repl-display-help-banner       . nil)
+((nil . ((cider-clojure-cli-global-options     . "-A:dev:test:chui")
+         (cider-custom-cljs-repl-init-form     . "(user/cljs-repl)")
+         (cider-default-cljs-repl              . custom)
+         (cider-preferred-build-tool           . clojure-cli)
          (cider-redirect-server-output-to-repl . t)
-         (cider-default-cljs-repl              . node)))
+         (cider-repl-display-help-banner       . nil)))
+ (eval . (progn
+           (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
+           (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware")))
  (clojure . ((eval . (define-clojure-indent
                        (assoc 0)
                        (ex-info 0)
