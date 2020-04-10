5a8965c1fe2a76ce6ac23c6eea5800d25df42063
Arne Brasseur <arne@arnebrasseur.net>
2020-04-10 14:46:03 +0200

    Use the browser-test target, add shadow nrepl middleware

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,10 +3,10 @@
          (cider-default-cljs-repl              . custom)
          (cider-preferred-build-tool           . clojure-cli)
          (cider-redirect-server-output-to-repl . t)
-         (cider-repl-display-help-banner       . nil)))
- (eval . (progn
-           (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
-           (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware")))
+         (cider-repl-display-help-banner       . nil)
+         (eval . (progn
+                   (make-variable-buffer-local 'cider-jack-in-nrepl-middlewares)
+                   (add-to-list 'cider-jack-in-nrepl-middlewares "shadow.cljs.devtools.server.nrepl/middleware")))))
  (clojure . ((eval . (define-clojure-indent
                        (assoc 0)
                        (ex-info 0)
